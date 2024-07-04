52479cabc733ce06aa9ed86b0c2a156bd9c9800b
Leandro Correia Galdino <leandrocgaldino@gmail.com>
2024-07-03 22:22:53 -0300

    Add Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:memento_mori/app.dart';
+import 'package:memento_mori/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
