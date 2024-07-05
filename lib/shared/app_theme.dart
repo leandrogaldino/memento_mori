b8296a305af9a924ced91841f13bdce0595f58a0
Leandro Correia Galdino <62770546+leandrogaldino@users.noreply.github.com>
2024-07-05 15:31:51 -0300

    Update

--- lib/shared/app_theme.dart
+++ lib/shared/app_theme.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 
 final class AppTheme {
-  static const orangeColor = Color.fromARGB(255, 255, 174, 69);
-  static const lightOrangeColor = Color.fromARGB(255, 255, 239, 233);
-  static const blueColor = Color.fromARGB(255, 1, 189, 214);
-  static const lightGreyColor = Color.fromARGB(255, 235, 235, 235);
+  static const primaryColor = Color.fromARGB(255, 153, 0, 0);
+  static const secondaryColor = Color.fromARGB(255, 255, 200, 200);
+  static const terciaryColor = Color.fromARGB(255, 51, 51, 51);
+  static const outlineColor = Color.fromARGB(255, 235, 235, 235);
 
   static final _defaultInputBorder = OutlineInputBorder(
     borderRadius: BorderRadius.circular(16),
-    borderSide: const BorderSide(color: lightGreyColor),
+    borderSide: const BorderSide(color: outlineColor),
   );
 
   static final lightTheme = ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: lightOrangeColor),
+    colorScheme: ColorScheme.fromSeed(seedColor: secondaryColor),
     useMaterial3: true,
     fontFamily: 'Raleway',
-    scaffoldBackgroundColor: orangeColor,
+    scaffoldBackgroundColor: primaryColor,
     inputDecorationTheme: InputDecorationTheme(
       filled: true,
-      fillColor: lightOrangeColor,
+      fillColor: secondaryColor,
       labelStyle: const TextStyle(
         fontSize: 14,
-        color: orangeColor,
+        color: primaryColor,
         fontWeight: FontWeight.w700,
       ),
       floatingLabelStyle: const TextStyle(
-        color: blueColor,
+        color: terciaryColor,
         fontWeight: FontWeight.w600,
       ),
       enabledBorder: _defaultInputBorder,
@@ -37,7 +37,7 @@ final class AppTheme {
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
           foregroundColor: Colors.white,
-          backgroundColor: blueColor,
+          backgroundColor: terciaryColor,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(16),
           ),
@@ -52,20 +52,20 @@ final class AppTheme {
   static final darkTheme = lightTheme;
 
   static const titleStyle = TextStyle(
-    color: blueColor,
+    color: terciaryColor,
     fontSize: 32,
     fontWeight: FontWeight.w900,
     fontFamily: 'Raleway',
   );
   static const titleSmallStyle = TextStyle(
-    color: blueColor,
+    color: terciaryColor,
     fontSize: 24,
     fontWeight: FontWeight.w900,
     fontFamily: 'Raleway',
   );
 
   static const subTitleStyle = TextStyle(
-    color: blueColor,
+    color: terciaryColor,
     fontSize: 18,
     fontWeight: FontWeight.w500,
     fontFamily: 'Raleway',
